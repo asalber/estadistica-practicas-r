11ca5511968ca74b15dfac657ea02d65f9c842f8
Alfredo Sánchez Alberca <asalber@gmail.com>
2025-01-26 17:50:12 +0100

    Añadida práctica de tipos de datos y reorganización de práctica de preprocesamiento.

--- prueba.R
+++
@@ -1,8 +0,0 @@
-set.seed(123)
-x = round(rnorm(15, 8, 0.4),1)
-y = round(rnorm(15, 7.5, 0.3),1)
-z = round(rnorm(15, 7.7, 0.35),1)
-
-df <- data.frame(vinoA=x, vinoB=y, vinoC=z)
-
-write.csv(df, "datos/cata-vinos.csv", row.names = FALSE)
